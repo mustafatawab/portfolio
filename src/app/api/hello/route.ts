3d6c3bd4eeeb470a4b9eef117925deeb4de104b9
mustafatawab <engrmustafatawab05@gmail.com>
2025-04-23 07:00:38 +0500

    working on Resend email

---
+++ src/app/api/hello/route.ts
@@ -0,0 +1,6 @@
+import { NextRequest, NextResponse } from "next/server"
+export async function GET(req : NextRequest, res : NextResponse){
+    return NextResponse.json({
+        message : "Hello !!! Welcome here "
+    })
+}
