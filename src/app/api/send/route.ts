92290637789e5cd89b427422733456fda6f9f09c
mustafatawab <engrmustafatawab05@gmail.com>
2025-04-27 22:08:29 +0500

    resend api

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -1,22 +1,22 @@
-import { NextResponse, NextRequest } from "next/server";
-import {Resend} from "resend"
-import EmailTemplate from "@/components/EmailTemplate/EmailTemplate";
+// import { NextResponse, NextRequest } from "next/server";
+// import {Resend} from "resend"
+// import EmailTemplate from "@/components/EmailTemplate/EmailTemplate";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+// const resend = new Resend(process.env.RESEND_API_KEY);
 
 
-export async function POST(request:NextRequest) {
+// export async function POST(request:NextRequest) {
    
-    try {
-        const data = await resend.emails.send({
-          from: 'Your Website <onboarding@resend.dev>',
-          to: ['example@gmail.com'], // Replace with your admin email
-          subject: `New message from ${name}`,
-          react: EmailTemplate({ name, email, message }),
-        });
+//     try {
+//         const data = await resend.emails.send({
+//           from: 'Your Website <onboarding@resend.dev>',
+//           to: ['example@gmail.com'], // Replace with your admin email
+//           subject: `New message from ${name}`,
+//           react: EmailTemplate({ name, email, message }),
+//         });
     
-        NextResponse.json(data , {status : 200});
-      } catch (error) {
-        res.status(500).json({ error });
-      }
-}
+//         NextResponse.json(data , {status : 200});
+//       } catch (error) {
+//         res.status(500).json({ error });
+//       }
+// }
