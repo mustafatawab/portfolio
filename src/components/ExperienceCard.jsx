57c436ea97d4b92059ea81dc7039c8db1e5f7563
mustafatawab <engrmustafatawab05@gmail.com>
2025-05-01 10:02:50 +0500

    resend integration

--- src/components/ExperienceCard.jsx
+++ src/components/ExperienceCard.jsx
@@ -9,11 +9,11 @@ import {
 const ExperienceCard = ({ experience }) => {
   const { company, to, from, designation, description } = experience;
   return (
-    <div className="md:flex gap-3 h-full w-full">
+    <div className="md:flex gap-3 h-full w-full ">
       <div className="md:block hidden bg-blue-300 w-[1px]  rounded-4xl  my-2 relative">
         <div className="rounded-full bg-blue-600 w-2 h-2 absolute -top-1 -left-1"></div>
       </div>
-      <Card className="shadow-none w-full hover:bg-blue-100">
+      <Card className="shadow-none w-full hover:bg-blue-100  dark:hover:bg-blue-700">
         <CardContent className="space-y-2">
           <div className="flex justify-between items-center">
             <CardTitle className="text-lg"> {designation}</CardTitle>
