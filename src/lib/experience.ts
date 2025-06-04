88bf276ecdfff7570cbe7a8984b2e37c42c40bc6
mustafatawab <engrmustafatawab05@gmail.com>
2025-06-04 16:40:04 +0500

    centered the experience section

--- src/lib/experience.ts
+++ src/lib/experience.ts
@@ -16,11 +16,11 @@ export const experiences = [
     },
 
 
-    {
-        company : 'Panacloud',
-        from : "Jan 2023",
-        to : "Nov 2023",
-        designation : "Full Stack Developer",
-        description : "It's like a program for youth to learn IT skills and I did. Nextjs as a full stack web framework with mongodb connectivity. Created full stack web app first time which was e-commerce store. Designed multiple landing pages and portofolio sites using tailwind css and typescript. Integration of some headless CMS (Sanity , Contentfull , Payload CMS, Strapi). Integration of strip payment integration in to e-commerce store"
-    }
+    // {
+    //     company : 'Panacloud',
+    //     from : "Jan 2023",
+    //     to : "Nov 2023",
+    //     designation : "Full Stack Developer",
+    //     description : "It's like a program for youth to learn IT skills and I did. Nextjs as a full stack web framework with mongodb connectivity. Created full stack web app first time which was e-commerce store. Designed multiple landing pages and portofolio sites using tailwind css and typescript. Integration of some headless CMS (Sanity , Contentfull , Payload CMS, Strapi). Integration of strip payment integration in to e-commerce store"
+    // }
 ]
--- src/views/experience.jsx
+++ src/views/experience.jsx
@@ -8,8 +8,8 @@ const Experience = () => {
         <main className="">
 
           <div className="space-y-2">
-            <h2 className="text-4xl font-bold px-5">Experience</h2>
-            <div className=" md:2/3 xl:w-1/2 space-y-5">
+            <h2 className="text-4xl font-bold px-5 text-center">Experience</h2>
+            <div className="mx-auto md:2/3 xl:w-1/2 space-y-5">
               {experiences.map((experience, index) => <div className="w-full" key={index}>
                 <ExpericeCard experience={experience} />
               </div>
